513f22d0aebcfcd2b5db325076c6708a17321880
Franci Penov <francip@gmail.com>
2024-02-23 00:13:13 -0800

    Misc reformat

--- brain-editor/webpack.main.config.cjs
+++ brain-editor/webpack.main.config.cjs
@@ -1,15 +1,24 @@
-const rules = require('./webpack.rules.cjs');
-const plugins = require('./webpack.plugins.cjs');
+const rules = require("./webpack.rules.cjs");
+const plugins = require("./webpack.plugins.cjs");
 
 const mainConfig = {
-  entry: './src/main.ts',
-  module: {
-    rules,
-  },
-  plugins,
-  resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.sass','.scss', '.json'],
-  },
+    entry: "./src/main.ts",
+    module: {
+        rules,
+    },
+    plugins,
+    resolve: {
+        extensions: [
+            ".js",
+            ".ts",
+            ".jsx",
+            ".tsx",
+            ".css",
+            ".sass",
+            ".scss",
+            ".json",
+        ],
+    },
 };
 
 module.exports = mainConfig;
